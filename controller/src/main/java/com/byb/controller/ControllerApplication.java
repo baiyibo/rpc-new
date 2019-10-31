d6cc892c567b168f3dd41040cca2cbcfd1c8bcf5
Baiyibo <236182321@qq.com>
2019-10-31 20:43:45 +0800

    6666666666

--- controller/src/main/java/com/byb/controller/ControllerApplication.java
+++ controller/src/main/java/com/byb/controller/ControllerApplication.java
@@ -2,8 +2,10 @@ package com.byb.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class ControllerApplication {
 
 	public static void main(String[] args) {
--- readme.txt
+++ readme.txt
@@ -1,6 +1,7 @@
 docker run -d \
--p 8080:8080 \
--e dubbo.registry.address=zookeeper://192.168.30.137:2181 \
+--name dubbo-admin \
+-p 8082:8080 \
+-e dubbo.registry.address=zookeeper://129.204.183.175:32771 \
 -e dubbo.admin.root.password=root \
 -e dubbo.admin.guest.password=guest \
 chenchuxin/dubbo-admin
